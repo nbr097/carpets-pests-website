2a3bb88acfed6cf1863b16835517fa7b7a50dd3e
nbr097 <nicho.brown2@gmail.com>
2023-11-06 12:30:38 +1000

    test updates

--- src/lib/Testimonials.svelte
+++ src/lib/Testimonials.svelte
@@ -11,6 +11,122 @@
 	</div>
 
 	<div class="grid md:grid-cols-3 gap-6 lg:gap-12 text-center">
+		<div class="mb-12 md:mb-0">
+			<div class="flex justify-center mb-6">
+				<span
+					class="inline-block h-32 w-32 rounded-full overflow-hidden bg-slate-50 border-4 dark:border-green-500 border-blue-500"
+				>
+				
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<img class="h-full" src="https://lh3.googleusercontent.com/a-/ACNPEu-suxCY6Nu340KPLUpiFMmG_eqxLCkJQe2PneV4Pg=w60-h60-p-rp-mo-br100">
+				</span>
+			</div>
+			<h5 class="text-xl font-semibold mb-4">Meegan</h5>
+			<h6 class="font-semibold dark:text-blue-400 text-zinc-800 mb-4">Customer</h6>
+			<p class="mb-4 text-justify">
+				<svg
+					aria-hidden="true"
+					focusable="false"
+					data-prefix="fas"
+					data-icon="quote-left"
+					class="w-6 pr-2 inline-block"
+					role="img"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512"
+				>
+					<path
+						fill="currentColor"
+						d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+					/></svg
+				>Tony is a legend! Arriving early to do carpet cleaning and fleas, he saved the day when our bond cleaner bailed! Tony's work ethic and friendly personality lifted our spirits and his willingness to help out wherever needed, was just amazing! We can never thank you enough Tony!</p>
+			<ul class="flex justify-center mb-0">
+				<li>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="star"
+						class="w-4 text-yellow-500"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 576 512"
+					>
+						<path
+							fill="currentColor"
+							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+						/>
+					</svg>
+				</li>
+				<li>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="star"
+						class="w-4 text-yellow-500"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 576 512"
+					>
+						<path
+							fill="currentColor"
+							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+						/>
+					</svg>
+				</li>
+				<li>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="star"
+						class="w-4 text-yellow-500"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 576 512"
+					>
+						<path
+							fill="currentColor"
+							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+						/>
+					</svg>
+				</li>
+				<li>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="star"
+						class="w-4 text-yellow-500"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 576 512"
+					>
+						<path
+							fill="currentColor"
+							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+						/>
+					</svg>
+				</li>
+				<li>
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fas"
+						data-icon="star"
+						class="w-4 text-yellow-500"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 576 512"
+					>
+						<path
+							fill="currentColor"
+							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+						/>
+					</svg>
+				</li>
+			</ul>
+		</div>
 		<div class="mb-0">
 			<div class="flex justify-center mb-6">
 				<span
@@ -243,122 +359,6 @@
 				</li>
 			</ul>
 		</div>
-		<div class="mb-12 md:mb-0">
-			<div class="flex justify-center mb-6">
-				<span
-					class="inline-block h-32 w-32 rounded-full overflow-hidden bg-slate-50 border-4 dark:border-green-500 border-blue-500"
-				>
-				
-				<!-- svelte-ignore a11y-missing-attribute -->
-				<img class="h-full" src="https://lh3.googleusercontent.com/a-/ACNPEu-suxCY6Nu340KPLUpiFMmG_eqxLCkJQe2PneV4Pg=w60-h60-p-rp-mo-br100">
-				</span>
-			</div>
-			<h5 class="text-xl font-semibold mb-4">Meliss</h5>
-			<h6 class="font-semibold dark:text-blue-400 text-zinc-800 mb-4">Customer</h6>
-			<p class="mb-4 text-justify">
-				<svg
-					aria-hidden="true"
-					focusable="false"
-					data-prefix="fas"
-					data-icon="quote-left"
-					class="w-6 pr-2 inline-block"
-					role="img"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 512 512"
-				>
-					<path
-						fill="currentColor"
-						d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
-					/></svg
-				>Tony's Carpet cleaning is the best ive used in a long time. He really takes pride in his work, and went above and beyond to help! The carpets came up "beautiful" - "thankyou Tony!"
-			</p>
-			<ul class="flex justify-center mb-0">
-				<li>
-					<svg
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="star"
-						class="w-4 text-yellow-500"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 576 512"
-					>
-						<path
-							fill="currentColor"
-							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-						/>
-					</svg>
-				</li>
-				<li>
-					<svg
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="star"
-						class="w-4 text-yellow-500"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 576 512"
-					>
-						<path
-							fill="currentColor"
-							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-						/>
-					</svg>
-				</li>
-				<li>
-					<svg
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="star"
-						class="w-4 text-yellow-500"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 576 512"
-					>
-						<path
-							fill="currentColor"
-							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-						/>
-					</svg>
-				</li>
-				<li>
-					<svg
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="star"
-						class="w-4 text-yellow-500"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 576 512"
-					>
-						<path
-							fill="currentColor"
-							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-						/>
-					</svg>
-				</li>
-				<li>
-					<svg
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="star"
-						class="w-4 text-yellow-500"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 576 512"
-					>
-						<path
-							fill="currentColor"
-							d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-						/>
-					</svg>
-				</li>
-			</ul>
-		</div>
+		
 	</div>
 </section>
