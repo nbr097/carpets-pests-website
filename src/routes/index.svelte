2cda6ee3a9e016d5eedd591558e9733296166d5e
nbr097 <nicho.brown2@gmail.com>
2023-11-27 18:11:37 +1000

    bug fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Testimonials from '$lib/Testimonials.svelte';
+    import Testimonials from '$lib/Testimonials.svelte';
     import Hero from '$lib/Hero.svelte';
     import Information from '$lib/Information.svelte';
     import Carousel from '$lib/svelte-carousel';
@@ -7,18 +7,19 @@
 
 <svelte:head>
     <title>Tony's Carpet & Pests</title>
-    <script type="application/ld+json">{
-        "@context": "http://schema.org",
-        "@type": "WebSite",
-          "name": "Tony's Carpet & Pests",
-          "url": "https://carpetandpests.com.au",
-        }</script>
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "WebSite",
+            "name": "Tony's Carpet & Pests",
+            "url": "https://carpetandpests.com.au"
+        }
+    </script>
     <meta name="description" content="Tony's Carpet & Pests is a local company based in Burpengary that offers a wide range of services from carpet cleaning, bond cleaning and pest control to a wide range of different suburbs.">
-        <meta name="robots" content="index,follow">
-        <meta name="googlebot" content="index,follow">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index,follow">
+    <meta name="googlebot" content="index,follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </svelte:head>
-
 
 <Hero />
 <Carousel/>
