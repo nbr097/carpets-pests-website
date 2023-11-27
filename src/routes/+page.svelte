fecf25f1d0afef622ca62ac3554cf9e17200fb84
nbr097 <nicho.brown2@gmail.com>
2023-11-27 18:17:05 +1000

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
 	import Testimonials from '$lib/Testimonials.svelte';
-    import Hero from '$lib/Hero.svelte';
+    import Hero from '$lib/Hero.svelte'; 
     import Information from '$lib/Information.svelte';
-	import { comma } from 'postcss/lib/list';
 </script>
 
 <svelte:head>
@@ -19,6 +18,6 @@
     <meta name="description" content="Tony's Carpet & Pests is a local company based in Burpengary that offers a wide range of services from carpet cleaning, bond cleaning and pest control to a wide range of different suburbs.">
 </svelte:head>
 
-<Hero />
+<Hero/>
 <Information/>
 <Testimonials/>
